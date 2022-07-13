3d2ae5c7c9ac6400a6b09bff0c3f927725afe638
Ho Yao Lun <67943288+ravonho@users.noreply.github.com>
2022-07-13 15:26:15 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,36 @@
+let songs = [
+{
+    name: 'song 1',
+    path: 'assets/song1.mp3',
+    artist: 'artist 1',
+    cover: 'img/cover1.jpeg'
+},
+
+{
+    name: 'song 2',
+    path: 'assets/song2.mp3',
+    artist: 'artist 2',
+    cover: 'img/cover2.jpeg'
+},
+
+{
+    name: 'song 3',
+    path: 'assets/song3.mp3',
+    artist: 'artist 3',
+    cover: 'img/cover3.jpeg'
+},
+
+{
+    name: 'song 4',
+    path: 'assets/song4.mp3',
+    artist: 'artist 4',
+    cover: 'img/cover4.jpg'
+},
+
+{
+    name: 'song 5',
+    path: 'assets/song5.mp3',
+    artist: 'artist 5',
+    cover: 'img/cover5.jpg'
+},
+]
